9d7a9f457275884746cbec0963ff0e6c34f4615c
asadaboontham <se57160514@gmail.com>
2017-05-01 14:50:55 +0700

    can login to server 2 //asada

--- app/src/main/java/com/example/asadaboomtham/stmdefectapplication/LoginActivity.java
+++ app/src/main/java/com/example/asadaboomtham/stmdefectapplication/LoginActivity.java
@@ -65,6 +65,7 @@ public class LoginActivity extends AppCompatActivity {
                                     .create()
                                     .show();
 
+
                         }
                     }
                     @Override
