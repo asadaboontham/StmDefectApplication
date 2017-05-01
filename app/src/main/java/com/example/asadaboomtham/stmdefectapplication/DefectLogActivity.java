f0b55901962434b5e894459b1a76e994d3ddb9b2
aod <se57160023aod@gmail.com>
2017-05-01 18:52:31 +0700

    หน้า layout defect_log เพิ่ม ไอคอน ถังขยะ และ send ตรงแถบ menu bar เหลือ การ service

--- app/src/main/java/com/example/asadaboomtham/stmdefectapplication/DefectLogActivity.java
+++ app/src/main/java/com/example/asadaboomtham/stmdefectapplication/DefectLogActivity.java
@@ -2,6 +2,7 @@ package com.example.asadaboomtham.stmdefectapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class DefectLogActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class DefectLogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_defect_log);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
