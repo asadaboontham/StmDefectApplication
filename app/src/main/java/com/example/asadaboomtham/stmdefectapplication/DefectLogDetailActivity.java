6210715896c98e49fde15b6d15ee694675ee604d
aod <se57160023aod@gmail.com>
2017-05-12 23:47:48 +0700

    เพิ่ม ปุ่ม icon แก้ไข ตรง iconbar หน้าคำอธิบาย

--- app/src/main/java/com/example/asadaboomtham/stmdefectapplication/DefectLogDetailActivity.java
+++ app/src/main/java/com/example/asadaboomtham/stmdefectapplication/DefectLogDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.asadaboomtham.stmdefectapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class DefectLogDetailActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class DefectLogDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_defect_log_detail);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
